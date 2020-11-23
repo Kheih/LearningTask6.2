9ce78f7c8bb706c8b4584290198a74fd39c2b516
Kheih <56195049+Kheih@users.noreply.github.com>
2020-11-23 18:34:18 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+</head>
+<body>
+<svg width="300" height="300" stroke="red" style="border:1px solid black">
+<svg xmlns="http://www.w3.org/2000/svg"  x="25" y="25" height="256" width="256" viewBox="-8 -8 16 16">
+<defs><filter id="f"><feGaussianBlur stdDeviation="0.2"/></filter></defs><g>
+<animateTransform attributeName="transform" type="rotate" dur="6s" to="360" repeatCount="indefinite"/>
+<path d="m0-7a3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 0 0 7 7 7 0 1 1 0-14z" fill="hsla(255,255%,255%,1)"/>
+<path d="m0-7a7 7 0 1 1 0 14 3.5 3.5 0 1 1 0-7 3.5 3.5 0 1 0 0-7z" fill="hsla(0,0%,0%,1)"/>
+<circle cy="-3.5" r="1.1" fill="hsla(0,0%,0%,1)"/>
+<circle cy="3.5" r="1.1" fill="hsla(255,255%,255%,1)"/>
+</g></svg>
+</body>
+</html>
